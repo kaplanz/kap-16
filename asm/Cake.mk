af82fcef8c19c01db535e094d4d9ef39ed5951b0
Zakhary Kaplan <zakharykaplan@gmail.com>
2021-07-19 19:45:00 -0400

    Create asm/

---
+++ asm/Cake.mk
@@ -0,0 +1,11 @@
+# Package
+NAME    = asm
+VERSION = 0.1.0
+AUTHOR  = Zakhary Kaplan <zakharykaplan@gmail.com>
+
+# Build
+CONFIG ?= BASIC
+
+# Extensions
+.cc = .cc
+.h = .h
---
+++ asm/src/main.cc
@@ -0,0 +1,11 @@
+// File:        main.cc
+// Author:      Zakhary Kaplan <https://zakharykaplan.ca>
+// Created:     19 Jul 2021
+// Version:     0.1.0
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+}
